09078b2756e17384bdc44a5c4d2ff28ef0dd93d2
Dan Brown <dan@thesplitpixel.com>
2025-05-29 17:14:27 -0500

    Refactor footer testimonials to not require count picking

--- partials/testimonials/testimonials-footer.php
+++ partials/testimonials/testimonials-footer.php
@@ -1,24 +1,19 @@
 <!--testimonials-footer-->
-<?php $count = get_field('testimonial_count');?>
-	<?php $query = new WP_Query( [
-    'post_type'      => 'testimonials',
-    'nopaging'       => false,
-    'posts_per_page' => $count
-] ); ?>
+<?php 
+$footer_testimonials = get_field('choose_testimonials');
+if( $footer_testimonials ) : ?>
 
-<div class="testimonials testimonials__background_<?php if(get_field('testimonial_count') == 1) : the_field('testimonial_background'); else : ?>gold<?php endif; ?> testimonial__count_<?php echo $count; ?>">
+<div class="testimonials testimonials__background_<?php if(get_field('testimonial_background')) : the_field('testimonial_background'); else : ?>gold<?php endif; ?> testimonial__count_<?php echo $count; ?>">
 	<div class="testimonials__content row wrap">
-        <?php if ( $query->have_posts() ) : ?>
-            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-            <?php if ($count != 1) : ?>
-                <div class="xs-col-12 <?php if ($count == 3) : ?>col-md-4 <?php else : ?>col-md-6<?php endif; ?> testimonials__multi">
-            <?php endif; ?>
-                <h3><?php the_content(); ?></h3>
-                <div class="attribution"><?php the_title(); ?></div>
-                <?php if ($count != 1) : ?>
+        <?php foreach ( $footer_testimonials as $post) : setup_postdata($post); ?>
+                <div class="testimonial">
+                    <img class="quote_mark" src="<?php echo get_template_directory_uri() . '/library/images/marker/quote_no_border.png'; ?>">
+                        <h3><?php the_content(); ?></h3>
+                        <div class="attribution"><?php the_title(); ?></div>
                 </div>
-                <?php endif; ?>
-            <?php endwhile; ?>
-        <?php endif; ?>
+                
+            <?php endforeach; ?>
     </div>
+    <?php wp_reset_postdata(); ?>
 </div>
+<?php endif; ?>
