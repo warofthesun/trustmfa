1c8821dd8a30292ba6d4997064acba808c73e319
Dan Brown <dan@thesplitpixel.com>
2025-06-01 14:21:43 -0500

    Refine transition out of mobile header when resizing on desktop

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -1,15 +1,6 @@
 /*
- * Bones Scripts File
- * Author: Eddie Machado
- *
- * This file should contain any js scripts you want to add to the site.
- * Instead of calling it in the header or throwing it inside wp_head()
- * this file will be called automatically in the footer so as not to
- * slow the page load.
- *
- * There are a lot of example functions and tools in here. If you don't
- * need any of it, just remove it. They are meant to be helpers and are
- * not required. It's your world baby, you can do whatever you want.
+ * Starter Scripts File
+
 */
 
 
@@ -125,8 +116,22 @@ jQuery(document).ready(function($) {
 
 	$("#mobile-nav").click(function(){
         $(".nav").toggleClass("show");
-				$("#mobile-nav").toggleClass("show");
-			});
+		$("#mobile-nav").toggleClass("show");
+	});
+
+	var $window = $(window),
+        $nav = $('.nav');
+
+    function resize() {
+        if ($window.width() > 1024) {
+            $("#mobile-nav").removeClass("show");
+			$(".nav").removeClass("show");
+        }
+    }
+
+    $window
+        .resize(resize)
+        .trigger('resize');
  
       
 	 $('.faq_question').click(function() {	 
