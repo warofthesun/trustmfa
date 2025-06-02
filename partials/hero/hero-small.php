8157381ccbd7a4a98dc95e0299e1c60fe339a9a1
Dan Brown <dan@thesplitpixel.com>
2025-06-02 11:08:03 -0500

    Refine small header curve

--- partials/hero/hero-small.php
+++ partials/hero/hero-small.php
@@ -5,7 +5,7 @@
 			<h1><?php if( get_field('page_title') ) : the_field('page_title'); else: single_post_title();  endif; ?></h1>
 		</div>
 	</div>
-	<div class="header_curve_lower"><?php echo file_get_contents( get_template_directory() . '/library/images/lower_blue_header_curve.svg' ); ?></div>
+	<!--div class="header_curve_lower"><?php //echo file_get_contents( get_template_directory() . '/library/images/lower_blue_header_curve.svg' ); ?></div-->
 	<div class="content_curve__outer">
 		<?php echo file_get_contents( get_template_directory() . '/library/images/content_curve__outer.svg' ); ?>
 	</div>
