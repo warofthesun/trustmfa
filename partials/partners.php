a366bbaabbf9dec9fbe42280278cda7ff0896ec7
Dan Brown <dan@thesplitpixel.com>
2025-06-13 11:53:50 -0500

    Add ability to link partner logo to partner website

--- partials/partners.php
+++ partials/partners.php
@@ -3,6 +3,9 @@
 <?php if( have_rows('highlighted_partners') ): ?>
 <div class="partners__container partners__container_highlighted row"> 
     <?php while ( have_rows('highlighted_partners') ) : the_row(); ?>
+    <?php $link = get_sub_field('partner_website'); ?>
+    <?php if( $link ): $link_url = $link['url']; $link_target = $link['target'] ? $link['target'] : '_self';?>
+    <a href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php endif; ?>
     <div class="partners">
         <?php
             $image = get_sub_field('partner_logo');
@@ -20,12 +23,13 @@
                 $width = $image['sizes'][ $size . '-width' ];
                 $height = $image['sizes'][ $size . '-height' ]; ?>
 
-            
+                
                 <img src="<?php echo $thumb; ?>" alt="<?php echo $alt_text; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" />
-            
+                
 
             <?php endif; ?>
     </div>
+    <?php if( $link ): ?></a><?php endif; ?>
     <?php endwhile; ?>
 </div>
 
@@ -40,10 +44,14 @@
 
 <div class="partners__container"> 
     <?php while ( have_rows('partners') ) : the_row(); ?>
+    <?php $link = get_sub_field('partner_website'); ?>
+    <?php if( $link ): $link_url = $link['url']; $link_target = $link['target'] ? $link['target'] : '_self';?>
+    <a href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php endif; ?>
     <div class="partners">
         <?php
             $image = get_sub_field('partner_logo');
             $alt_text = get_sub_field('partner_name');
+            
 
             if( !empty($image) ):
             // vars
@@ -57,12 +65,13 @@
                 $width = $image['sizes'][ $size . '-width' ];
                 $height = $image['sizes'][ $size . '-height' ]; ?>
 
-            
+                
                 <img src="<?php echo $thumb; ?>" alt="<?php echo $alt_text; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" />
             
 
             <?php endif; ?>
     </div>
+    <?php if( $link ): ?></a><?php endif; ?>
     <?php endwhile; ?>
 </div>
 
